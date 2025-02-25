890a126c0ecd6a4b9da684a6898f0e6c4f92e840
Hanan Zwaihed <hananzw30@gmail.com>
2025-02-25 22:19:43 +0200

    Added my React code to frontend folder ....

---
+++ frontend/README.md
@@ -0,0 +1,23 @@
+
+## Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/HananZ29/LNA_FRONTEND.git
+
+# Navigate to the project directory
+cd LNA_FRONTEND
+
+# Install dependencies
+npm install
+
+# Run using: 
+npm run dev
+
+## project screenshots
+![Screenshot]("https://github.com/user-attachments/assets/58ab1b01-3abf-4520-89a4-93c9db8fccb9")
+![Screenshot]("https://github.com/user-attachments/assets/6716331b-3eb1-4799-817a-35611b81f31a")
+
+
+
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import  React from 'react';
+import { useState } from 'react';
+import SearchAppBar from './components/Appbar';
+import NewsStack from './components/NewsStack';
+import { Box } from '@mui/material';
+
+export default function App() {
+  const [language, setLanguage] = useState<'en' | 'ar'>('en');
+
+  return (
+    <Box sx={{ 
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      direction: language === 'ar' ? 'rtl' : 'ltr'
+    }}>
+      <SearchAppBar language={language} setLanguage={setLanguage} />
+      
+      
+      <Box component="main" sx={{ 
+        flexGrow: 1,
+        marginTop: '64px', 
+        padding: 3
+      }}>
+        <NewsStack language={language} />
+      </Box>
+    </Box>
+  );
+}

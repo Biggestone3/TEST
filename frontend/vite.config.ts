fc53027170aad6dd31c3c0b41a02d6ce6ee20fa1
HananZ <145458776+HananZ29@users.noreply.github.com>
2025-03-11 00:43:38 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 
 import react from '@vitejs/plugin-react-swc'
-=======
+
 import react from '@vitejs/plugin-react'
->>>>>>> a67de4a2259f5daff532e88dda9143329b05b009
+
 
 // https://vite.dev/config/
 export default defineConfig({
